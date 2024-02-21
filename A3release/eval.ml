a1f4e57bb840c427b76a31680e1474448fc4f565
christopher <cjl323@cornel.edu>
2024-02-21 10:55:41 -0500

    Fixed evalA and evalB. Started the new logic for evalC

--- A3release/eval.ml
+++ A3release/eval.ml
@@ -16,13 +16,16 @@ and value=
 | VBool of bool
 | VString of string
 
-(*The configuration is a map from a store to an expression*)
+(*The configuration is formatted in the same as way as the instructions
+   it looks as follows <store, c, c', k> where k is a list of pair (cb, cc)*)
 type configuration = store*com*com*(com*com list)
 
 
 (* Create an initial configuration from a command. *)
 let make_configuration (c:com) : configuration = (empty_store, c, Skip, [])
 
+
+(*******************************Evaluating an aexp****************************)
 let is_valueA: aexp->bool = function
   | Int _-> true
   | Var _-> true
@@ -33,61 +36,87 @@ let rec stepAexp : aexp -> aexp=function
   | Int i-> failwith "Do not step"
   | Var v-> failwith "Do not step"
   | Input -> failwith "No clue what to do here"
-  | Some (a1, a2) when is_valueA a1 && is_valueA a2 -> step_some_A Some a1 a2
-  | Some (a1, a2) when is_valueA a1-> Some (a1, stepAexp a2)
-  | Some (a1, a2) -> Some (stepAexp a1, a2)
+  | Plus (a1, a2) when is_valueA a1 && when is_valueA a2 -> Int(a1+a2)
+  | Plus (a1, a2) when is_valueA a1-> Plus (a1, stepAexp a2)
+  | Plus (a1, a2) -> Plus (stepAexp a1, a2)
+  | Times (a1, a2) when is_valueA a1 && when is_valueA a2 -> Int(a1*a2)
+  | Times (a1, a2) when is_valueA a1-> Times (a1, stepAexp a2)
+  | Times (a1, a2) -> Times (stepAexp a1, a2)
+  | Minus (a1, a2) when is_valueA a1 && when is_valueA a2 -> Int(a1-a2)
+  | Minus (a1, a2) when is_valueA a1-> Minus (a1, stepAexp a2)
+  | Minus (a1, a2) -> Minus (stepAexp a1, a2)
 
-  and step_some_A some a1 a2 =
-    match some, a1, a2 with
-    | Plus, Int a1, Int a2 -> Int(a1+a2)
-    | Times, Int a1, Int a2 -> Int(a1*a2)
-    | Minus, Int a1, Int a2 -> Int(a1-a2)
-
-(*evaluate an aexp*)
 let rec evalAexp (e: aexp): value=
   if is_valueA e then e else
     e|> stepAexp |> evalAexp
 
+
+(*******************************Evaluating a bexp*****************************)
+
+(*Checking that bexp is a value*)
 let is_valueB: bexp->bool = function
   | True-> true
   | False-> true
   | _-> false
 
+(*When b is a value we need to actually extract the bool from the constructor*)
+let boolB: bexp->bool=function
+  | True->true
+  | False->false
+  | _-> failwith "Should never come here"
+
+let fromBval: bool->bexp=function
+  | true-> True
+  | false-> False
+
 let rec stepBexp: bexp->bexp=function
-  | True -> failwith "Do Not Step"
-  | False -> failwith "Do Not Step"
-  | Not b when is_valueB b-> Bool(!b)
+  | True -> true
+  | False -> false
+  | Not b when is_valueB b-> fromBval (not(boolB b))
   | Not b-> stepBexp b
-  | And (b1, b2) when is_valueB b1 && is_valueB b2-> Bool(b1 && b2)
+  | And (b1, b2) when is_valueB b1 && is_valueB b2->  fromBval ((boolB b1) && (boolB b2))
   | And (b1, b2) when is_valueB b1-> And(b1, stepBexp b2)
   | And (b1, b2)-> And(stepBexp b1, b2)
-  | Or (b1, b2) when is_valueB b1 && is_valueB b2-> Bool(b1 || b2)
+  | Or (b1, b2) when is_valueB b1 && is_valueB b2-> fromBval ((boolB b1) || (boolB b2))
   | Or (b1, b2) when is_valueB b1-> Or(b1, stepBexp b2)
   | Or (b1, b2)->Or(stepBexp b1, b2)
-  | Some (a1, a2) when is_valueA a1 && is_valueA a2 -> step_some_B Some a1 a2
-  | Some (a1, a2) when is_valueA a1-> Some (a1, evalAexp a2)
-  | Some (a1, a2) -> Some (evalAexp a1, a2)
-
-  and step_some_B some a1 a2=
-  match some, a1, a2 with
-  | Equals, Int a1, Int a2-> Bool(a1==a2)
-  | NotEquals, Int a1, Int a2-> Bool(a1!=a2)
-  | Less, Int a1, Int a2->Bool(a1<a2)
-  | LessEq, Int a1, Int a2->Bool(a1<=a2)
-  | Greater, Int a1, Int a2->Bool(a1>a2)
-  | GreaterEq, Int a1, Int a2->Bool(a1>=a2)
-
+  | Equals (a1, a2)-> fromBval ((evalAexp a1)==(evalAexp a2))
+  | NotEquals (a1, a2) -> fromBval ((evalAexp a1)!=(evalAexp a2))
+  | Less (a1, a2)-> fromBval ((evalAexp a1)<(evalAexp a2))
+  | LessEq (a1, a2)-> fromBval ((evalAexp a1)<=(evalAexp a2))
+  | Greater (a1, a2)-> fromBval ((evalAexp a1)>(evalAexp a2))
+  | GreaterEq (a1, a2)-> fromBval ((evalAexp a1)>=(evalAexp a2))
 
 
 let rec evalBexp (e: bexp): value=
-  if is_valueB e then e else
+  if is_valueB e then boolB e else
     e|> stepBexp |> evalBexp
     
-(* Evaluate a command. *)
-let rec evalc (conf:configuration) : store =
+(**************************** Evaluate a command*****************************)
+let is_complete: com->bool=function
+  | Skip->true
+  | _-> false
+
+(*When the conf is <store, Skip, Skip, k> then the program has terminated. 
+   Otherwise, we will run the first commind first and the second. *)
+let rec stepCom:configuration->configuration=function
   match conf with
-  | s, Skip, Skip, k-> s
-  | s, Assign (var, aexp), Skip, k->Store.add var aexp s
-  | s, Seq (c1, c2), Skip, k-> evalc s c1 Skip k and eval s c2 Skip k
+  | s, Skip, Skip, k-> (s, Skip, Skip, k)
+  | s, Skip, x, k-> (s, x, Skip, k)  
+
+  | s, Assign (var, aexp), x, k->(Store.add var (evalAexp aexp) s, Skip, x, k)
+
+  | s, Print(aexp), x, k-> pprintAexp(aexp); (s, Skip, x, k)
+
+  | s, If(b, c1, c2), x, k when is_valueB b && boolB b->(s, c1, x, k)
+  | s, If(b, c1, c2), x, k when is_valueB b && not(boolB b)-> (s, c2, x, k)
+  | s, If(b, c1, c2), x, k-> (s, If(evalBexp b, c1, c2), x, k)
+
+
+  | s, Seq (c1, c2), Skip, k-> 
   | s, Seq (c1, c2), Continue, k -> failwith "to do"
-  | _-> failwith "to do"
+
+let rec evalc (conf:configuration) : store =
+  match conf with 
+  | s, c1, c2, k-> if is_complete c1 && is_complete c2 then s 
+                    else conf|> stepCom |> evalc
